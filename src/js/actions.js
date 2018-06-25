7aa08f43f46638deea7ba0aa4dd2a82ee7314efe
james <james@autotelic.io>
2018-06-25 19:42:44 +1200

    add dummy middleware code for server calls

--- src/js/actions.js
+++ src/js/actions.js
@@ -3,6 +3,34 @@ import type {Dog} from "../ui.jsx";
 export const ADD_DOG = 'ADD_DOG';
 export const EDIT_DOG = 'EDIT_DOG';
 
+/***
+
+NOTE - Would call middleware to push changes to the server before verfiying and updating the store if successful ie.
+
+export function store(data) {
+  return dispatch =>
+    fetch('http://xxxxxxxxxxxxxxxx', {
+      ...
+    })
+    .then(response => {
+      if (response.status >= 200 && response.status < 300) {
+
+        dispatch(addDog(response));
+
+      } else {
+
+        const error = new Error(response.statusText);
+        error.response = response;
+        dispatch(errorAction(error));
+      }
+    })
+    .catch(error => { console.log('request failed', error); });
+}
+
+***/
+
+// for the purposes of this mockup we just call the actions directly, pretending that they have been stored successfully
+
 export function addDog(newDog:Dog) {
   return { type: ADD_DOG, newDog:newDog }
 }
--- src/js/reducers.js
+++ src/js/reducers.js
@@ -31,23 +31,18 @@ germanShepherd.id+=1;
 poodle.id+=2;
 
 const initialData = {
-  "dogs" : [spaniel, germanShepherd, poodle]
+  processing: false,
+  "dogs": [spaniel, germanShepherd, poodle]
 };
 
 export function dogs(state = initialData, action) {
   switch (action.type) {
     case ADD_DOG:
-
-      console.log(action.newDog.name + " was added");
-
       return Object.assign({}, state, {
         dogs: [...state.dogs, action.newDog]
       })
 
     case EDIT_DOG:
-
-      console.log(action.updatedDog.name + " was updated");
-
       return Object.assign({}, state, {
         dogs: state.dogs.map(
                (dog) => dog.id === action.updatedDog.id ? action.updatedDog : dog
