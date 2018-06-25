3a7be4bf55d88868e8cacbee347e5812c53653bc
james <james@autotelic.io>
2018-06-25 18:47:19 +1200

    add nav for touch devices

--- src/js/components/nav/nav.jsx
+++ src/js/components/nav/nav.jsx
@@ -2,8 +2,10 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import { withRouter, Link } from 'react-router-dom';
+import { Input } from 'reactstrap';
 import PropTypes from 'prop-types';
 import anime from 'animejs';
+import history from '../../history'
 
 import styles from './nav.css';
 
@@ -23,16 +25,28 @@ class Nav extends React.Component<Props>
     history: PropTypes.object.isRequired
   }
 
+  handleChange(event) {
+
+    history.push(event.target.value)
+  }
+
   render(){
     const { match, location, history } = this.props
 
     return (
       <nav className={"nav" + (this.props.specifier !== undefined ? ' ' + this.props.specifier : '')}>
-        {
-          this.props.paths.map((path) =>
+        {this.props.paths.map((path) =>
             <Link to={"/dog/" + (path.path)} className="nav__link">{path.name}</Link>
-          )
-        }
+        )}
+        <formgroup>
+          <label for="exampleSelect">Dogs</label>
+          <Input type="select" name="select" id="exampleSelect" onChange={(e) => this.handleChange(e)}>
+            <option value="" selected disabled hidden>Select a dog</option>
+            {this.props.paths.map((path) =>
+              <option value={"/dog/" + (path.path)}>{path.name}</option>
+            )}
+          </Input>
+        </formgroup>
       </nav>
     );
   }
